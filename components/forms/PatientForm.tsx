ea855a36b573715eafcc92e532d8835ed6917130
Elvismutinda <elvocool@gmail.com>
2024-07-18 14:09:50 +0300

    Updated placeholders on the patient form

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -4,22 +4,13 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { Form } from "@/components/ui/form";
-import CustomFormField from "../CustomFormField";
+import CustomFormField, { FormFieldType } from "../CustomFormField";
 import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
 import { createUser } from "@/lib/actions/patient.actions";
-
-export enum FormFieldType {
-  INPUT = "input",
-  TEXTAREA = "textarea",
-  PHONE_INPUT = "phoneInput",
-  CHECKBOX = "checkbox",
-  DATE_PICKER = "datePicker",
-  SELECT = "select",
-  SKELETON = "skeleton",
-}
+import "react-phone-number-input/style.css";
 
 const PatientForm = () => {
   const router = useRouter();
@@ -34,28 +25,29 @@ const PatientForm = () => {
     },
   });
 
-  async function onSubmit({
-    name,
-    email,
-    phone,
-  }: z.infer<typeof UserFormValidation>) {
+  const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
     setIsLoading(true);
 
     try {
       const userData = {
-        name,
-        email,
-        phone,
+        name: values.name,
+        email: values.email,
+        phone: values.phone,
       };
 
-      const user = await createUser(userData);
+      const newUser = await createUser(userData);
 
-      if (user) {
-        router.push(`/patients/${user.$id}/register`);
+      if (newUser) {
+        router.push(`/patients/${newUser.$id}/register`);
       }
+
+      console.log(userData);
+      // console.log({ newUser });
     } catch (error) {
       console.log(error);
     }
+
+    setIsLoading(false);
   }
 
   return (
@@ -71,7 +63,7 @@ const PatientForm = () => {
           control={form.control}
           name="name"
           label="Full name"
-          placeholder="John Doe"
+          placeholder="ex. John Doe"
           iconSrc="/assets/icons/user.svg"
           iconAlt="user"
         />
@@ -80,8 +72,8 @@ const PatientForm = () => {
           fieldType={FormFieldType.INPUT}
           control={form.control}
           name="email"
-          label="Email"
-          placeholder="johndoe@gmail.com"
+          label="Email address"
+          placeholder="ex. johndoe@gmail.com"
           iconSrc="/assets/icons/email.svg"
           iconAlt="email"
         />
@@ -91,7 +83,7 @@ const PatientForm = () => {
           control={form.control}
           name="phone"
           label="Phone number"
-          placeholder="+254 123 456789"
+          placeholder="ex. +254 123 456789"
         />
 
         <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
