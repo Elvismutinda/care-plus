3fa3ee112e677a74f726eca6ebfbbc845d199ded
Elvismutinda <elvocool@gmail.com>
2024-07-19 11:59:45 +0300

    Added search params for admin login

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 import { PatientForm } from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 
 const Home = ({ searchParams }: SearchParamProps) => {
+  const isAdmin = searchParams.admin === "true";
+
   return (
     <div className="flex h-screen max-h-screen">
-      {/* TODO: OTP Verification | Passkey Modal */}
+      {isAdmin && <PasskeyModal />}
       
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
